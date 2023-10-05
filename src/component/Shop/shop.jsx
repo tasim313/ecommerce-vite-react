fa4e2dd3399bca5eb652105c09b7a8960af7b0fb
tasim313 <mostasimmahmudtasim@gmail.com>
2023-10-05 14:41:39 +0600

    Shop component create

---
+++ src/component/Shop/shop.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+import './shop.css'
+import Product from "../Product/product";
+
+const Shop = () =>{
+    const [products, setProducts] = useState([]);
+    useEffect(()=>{
+        fetch('products.json')
+        .then(response => response.json())
+        .then(data => setProducts(data))
+    },[])
+    return(
+        <div className="shop-container">
+            <div className="products-container">
+                {
+                    products.map(product => <Product
+                    key={product.id}
+                    product = {product}
+                    ></Product>)
+                }
+            </div>
+            <div className="cart-container">
+                <h4>Order Summary</h4>
+            </div>
+
+        </div>
+    )
+}
+
+export default Shop
