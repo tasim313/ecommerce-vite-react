dbacd0f669dd9c7195264490f2ec52c42cb91ce9
tasim313 <mostasimmahmudtasim@gmail.com>
2023-10-10 17:18:42 +0600

    create new handelAddToCart function for product quantity

--- src/component/Shop/shop.jsx
+++ src/component/Shop/shop.jsx
@@ -30,8 +30,25 @@ const Shop = () =>{
         setCart(savedCart);
     }, [products])
 
+    // const handelAddToCart = (product) =>{
+    //     const newCart = [...cart, product]
+    //     setCart(newCart);
+    //     addToDb(product.id)
+    // }
+
     const handelAddToCart = (product) =>{
-        const newCart = [...cart, product]
+        // it is another way to handel product quantity.when user not select product quantity .quantity value is 0. That's why update quantity
+        let newCart = []
+        const exists = cart.find(pd => pd.id === product.id)
+        if(!exists){
+            product.quantity = 1;
+            newCart = [...cart, product]
+        }
+        else{
+            exists.quantity = exists.quantity + 1;
+            const remaining = cart.filter(pd => pd.id !== product.id);
+            newCart = [...remaining, exists];
+        }
         setCart(newCart);
         addToDb(product.id)
     }
