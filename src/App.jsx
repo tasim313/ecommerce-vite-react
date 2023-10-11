ee86681d1eacdff04a23f53569b33475bea03074
tasim313 <mostasimmahmudtasim@gmail.com>
2023-10-11 12:40:38 +0600

    import react-bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import './App.css'
 import Header from './component/Header/header'
 import Shop from './component/Shop/shop'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const [count, setCount] = useState(0)
